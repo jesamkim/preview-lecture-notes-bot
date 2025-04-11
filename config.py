d6f0664a1dc1b7c19b5c3d35539ef74a72c492d5
Jesam Kim <jesamkim@gmail.com>
2025-04-12 08:04:41 +0900

    update

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # Bedrock 설정
 BEDROCK_REGION = 'us-west-2'
-BEDROCK_MODEL_ID = 'us.anthropic.claude-3-5-sonnet-20241022-v2:0'
+#BEDROCK_MODEL_ID = 'us.anthropic.claude-3-5-sonnet-20241022-v2:0'
+BEDROCK_MODEL_ID = 'us.anthropic.claude-3-7-sonnet-20250219-v1:0'
 
 # 파일 경로 설정
 TEMP_DIR = './temp'
